468b68db658e3f0e51c298d1a91d6eea03c339eb
Vladimir Nemoguy <lightbringer1234@gmail.com>
2026-06-09 22:55:53 +0300

    homework

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -1,15 +1,29 @@
 /*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+Напишите Функцию `doubleEachCharacter`, которая Принимает СТРОКУ(string) и ВОЗВРАЩАЕТ(return) НОВУЮ СТРОКУ(newString),
+где каждый символ из ИСХОДНОЙ СТРОКИ ПОВТОРЯЕТСЯ ДВАЖДЫ(ДУБЛИРУЕТСЯ).
 
 Входные данные:
 
-- **`str`**: Строка, символы которой нужно удвоить.
+- **`str`**: СТРОКА, символы которой нужно УДВОИТЬ(ДУБЛИРОВАТЬ).
 
 Выходные данные:
 
-- Новая строка с каждым символом, повторенным дважды.
+- НОВАЯ СТРОКА(newString) с каждым символом, ПОВТОРЁННЫМ ДВАЖДЫ(ДУБЛИРОВАННЫМ).
 
-Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
+Пример работы Функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
+let string = "Hello";
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newString = ""; // Новая Пустая Строка для сбора результата
+
+    for (let i = 0; i < str.length; i++) { // 5 КРУТОК
+        let currentLetter = str[i]; // Хитро!🪙 Как бы БЕРЁМ ДАННЫЕ СТРОКИ И ПРЕОБРАЗУЕМ ИХ В ИНДЕКСЫ МАССИВА
+
+        newString = newString + currentLetter + currentLetter; // Хитро!🪙 КОНКАНТЕНАЦИЯ "" + str[i] + str[i]
+    }
+    return newString
+}
+
+let result = doubleEachCharacter(string)
+console.log(result);
