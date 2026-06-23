4e460c351ce7de74b1eefd899e19d3a63ae0228d
Vladimir Nemoguy <lightbringer1234@gmail.com>
2026-06-23 20:12:31 +0300

    homework

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,31 +1,33 @@
 /*
-Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+Напишите ФУНКЦИЮ `updateGallery`, которая будет ОБНОВЛЯТЬ ИНФОРМАЦИЮ о произведениях искусства(КАРТИНАХ) в виртуальной галерее.
+ФУНКЦИЯ `updateGallery` должна принимать ТРИ ПАРАМЕТРА(ТРИ АРГУМЕНТА):
 
-- объект галереи
-- название произведения (ключ)
-- новое значение
+- ОБЪЕКТ галереи(gallery {})
+- название КАРТИНЫ(КЛЮЧ)(СТРОКА)
+- НОВОЕ ЗНАЧЕНИЕ(СТРОКА)
 
-Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
+Если(if) КАРТИНА с таким названием УЖЕ ЕСТЬ(ДУБЛЬ👥) в галерее, его значение должно быть ОБНОВЛЕНО(return). 
+Если(if esle) КАРТИНЫ НЕТ, оно должно быть ДОБАВЛЕНО в ОБЪЕКТ галерею(ОТ ПРОТИВНОГО).
 
 Пример использования функции:
 
 const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch'
+  "Mona Lisa": "Leonardo da Vinci",
+  "Starry Night": "Vincent van Gogh",
+  "The Scream": "Edvard Munch"
 }
 
-updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
-updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+updateGallery(gallery, "Mona Lisa", "Leonardo da Vinci, 1503-1506")
+updateGallery(gallery, "The Persistence of Memory", "Salvador Dali")
 
 console.log(gallery)
 
 Ожидаемый вывод:
 {
-  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-  'The Persistence of Memory': 'Salvador Dali'
+  "Mona Lisa": "Leonardo da Vinci, 1503-1506",
+  "Starry Night": "Vincent van Gogh",
+  "The Scream": "Edvard Munch",
+  "The Persistence of Memory": "Salvador Dali"
 }
 */
 
@@ -35,4 +37,12 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, picture, artist) {
+  for (const key in gallery) {  // ПЕРЕБОР ВСЕХ КЛЮЧЕЙ в Объекте gallery
+    if (key === picture) { //  ПРОВЕРЯЕМ: совпадает ли текущий КЛЮЧ с тем, что мы ищем
+      gallery[picture] = artist; // ЕСЛИ НАШЛИ(true) — Обновляем значение у этой Картины
+      return; // ВЫХОДИМ из Функции(return), дальше код не выполняется
+    }
+  }
+  gallery[picture] = artist; // ХИТРО!🪙 СОЗДАЁМ НОВУЮ КАРТИНУ в Объекте, gallery['The Persistence of Memory'] = 'Salvador Dali'(ДОБАВИЛИ)
+}
