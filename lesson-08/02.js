23a15ee26c71d80baa26db257493d5745da5301b
Vladimir Nemoguy <lightbringer1234@gmail.com>
2026-07-21 18:12:32 +0300

    new

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,6 +4,19 @@
  * Если(if) Строка(Аргумент) является(===) Числом, Функцию(isNumeric) должна ВОЗВРАЩАТЬ(return) true, в противном случае - false.
  */
 
+  function isNumeric(str) {
+  // Убираем пробелы в начале и конце
+  let trimmed = str.trim();
+  
+  // Если строка пустая → не число
+  if (trimmed === '') {
+    return false;
+  }
+  
+  // Проверяем, что это число и не специальное значение
+  return !isNaN(trimmed) && isFinite(trimmed);
+}
+
 // function isNumeric(str) {
 //   let number = +str;
 //   if (number) {
@@ -17,9 +30,9 @@
 //   return +str ? true : false
 // }
 
-function isNumeric(str) {
-  return !isNaN(str) && str.trim() !== '';
-}
+// function isNumeric(str) {
+//   return !isNaN(str) && str.trim() !== '';
+// }
 
 console.log(isNumeric("123")) // Ожидаемый результат: true
 console.log(isNumeric("12.3")) // Ожидаемый результат: true
