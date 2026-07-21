1bd8b91f4f79d49455ad79109ef02c1e80be83ba
Vladimir Nemoguy <lightbringer1234@gmail.com>
2026-07-21 18:29:59 +0300

    new

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,38 +4,38 @@
  * Если(if) Строка(Аргумент) является(===) Числом, Функцию(isNumeric) должна ВОЗВРАЩАТЬ(return) true, в противном случае - false.
  */
 
-//   function isNumeric(str) {
-//   // Убираем пробелы в начале и конце
-//   let trimmed = str.trim();
-  
-//   // Если строка пустая → не число
-//   if (trimmed === '') {
-//     return false;
-//   }
-  
-//   // Проверяем, что это число и не специальное значение
-//   return !isNaN(trimmed) && isFinite(trimmed);
-// }
-
 function isNumeric(str) {
-  let number = +str;
-  if (number) {
-    return true
-  } else {
-    return false
-  }
+  return +str ? true : false
 }
-
-// function isNumeric(str) {
-//   return +str ? true : false
-// }
-
-// function isNumeric(str) {
-//   return !isNaN(str) && str.trim() !== '';
-// }
 
 console.log(isNumeric("123")) // Ожидаемый результат: true
 console.log(isNumeric("12.3")) // Ожидаемый результат: true
 console.log(isNumeric("123abc")) // Ожидаемый результат: false
 console.log(isNumeric("abc")) // Ожидаемый результат: false
 console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+//   function isNumeric(str) {
+//   // Убираем пробелы в начале и конце
+//   let trimmed = str.trim();
+
+//   // Если строка пустая → не число
+//   if (trimmed === '') {
+//     return false;
+//   }
+
+//   // Проверяем, что это число и не специальное значение
+//   return !isNaN(trimmed) && isFinite(trimmed);
+// }
+
+// function isNumeric(str) {
+//   let number = +str;
+//   if (number) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// function isNumeric(str) {
+//   return !isNaN(str) && str.trim() !== '';
+// }
